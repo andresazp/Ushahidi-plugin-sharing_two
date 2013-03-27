e16cbca868f14a3553cbc91e44e86f04e3bacbf1
Andres E. Azpurua <andresazp@gmail.com>
2013-03-26 21:31:39 -0430

    es_VE language for the plugin

---
+++ i18n/es_VE/sharing_two.php
@@ -0,0 +1,24 @@
+<?php
+$lang = array(
+	'all_sites' => 'Todas las Fuentes',
+	'site_filter' => 'Fuentes',
+	'view_original' => 'Abrir eporte original en: %s',
+	'share_reports_enabled' => 'Compartir Reportes: <strong>activado</strong>',
+	'share_categories_enabled' => 'Compartir Categorias: <strong>activado</strong>',
+	'share_reports' => 'Compartir reportes?',
+	'share_categories' => 'Compartir categorias?',
+	'site_color' => array(
+		'length' => 'El Color debe ser descrito en 6 caracteres.',
+		'required' => 'El campo Color es requerido.',
+	),
+	'site_name' => array(
+		'length' => 'El nombre de fuente a Compartir no parece ser v&aacute;lido.',
+		'required' => 'Un nombre de Compartir es necesario.',
+	),
+	'site_url' => array(
+		'exists' => 'El URL de fuente ya exieste.',
+		'length' => 'El URL de fuente no parece ser v&aacute;lido.',
+		'required' => 'Se requiere un URL de fuente.',
+		'url' => 'El campo URL de fuente no parece tener un URL válido.',
+	)
+);
